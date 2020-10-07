de76cf3c646d8f5ac8d2d385c37c997c994fc351
Stan <josestalyn@hotmail.com>
2020-10-07 16:11:12 -0500

    Docs agregado

---
+++ docs/precache-manifest.351d1f5b6b11dc625ad243b39f836235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3030121f3a1ff8864aa8127e5bc069c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723d914a7c2e4491a31a",
+    "url": "/static/css/main.a8153163.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723d914a7c2e4491a31a",
+    "url": "/static/js/main.82d7eed5.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
